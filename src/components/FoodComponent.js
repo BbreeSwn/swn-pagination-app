488169dee8c30df7118c428b4618e7d819e59a6b
Bree Swn <suwanan.cs@hotmail.com>
2025-01-31 12:38:04 +0700

    create food state

--- src/components/FoodComponent.js
+++ src/components/FoodComponent.js
@@ -1,7 +1,10 @@
-const FoodComponent = () => {
+const FoodComponent = ({name , image_url}) => {
   return (
-    <div><h1>FoodComponent</h1></div>
-  )
-}
+    <div>
+        <img src={image_url} alt={name} />
+      <h1>{name}</h1>
+    </div>
+  );
+};
 
-export default FoodComponent
+export default FoodComponent;
